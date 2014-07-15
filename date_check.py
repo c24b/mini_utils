9ce3797c871f41d419356bcd75c2399bc94cb11f
C2_4b <4barbes@gmail.com>
2014-07-15 11:36:01 +0200

    check format function for eah specific value

--- date_check.py
+++ date_check.py
@@ -19,3 +19,44 @@ def check_format(value, format=None):
 				return False
 			except Exception, e:
 				return False
+
+def check_month(month):	
+	month = month.capitalize()
+	try:
+		return datetime.datetime.strptime(month, r"%b")
+	except ValueError:
+		try:
+			return datetime.datetime.strptime(month, r"%B")
+		except ValueError:
+			return False
+
+def check_year(year):	
+	try:
+		return datetime.datetime.strptime(year,"%Y")
+	except ValueError:
+		try:
+			return datetime.datetime.strptime(month, r"%Y"))
+		except ValueError:
+			try:
+				return year.is_digit()
+			except ValueError:
+				return False
+
+def check_dow(dow):
+	try:
+		return datetime.datetime.strptime(year,"%a")
+		except ValueError:
+			try:
+				return datetime.datetime.strptime(month, r"%A"))
+			except ValueError:
+				year.is_digit()
+				return False
+
+def check_format(value, format="A"):
+	if format is None:
+		return value.is_digit()
+	else:
+		try:
+			return datetime.datetime.strptime(value,str("%"+format))
+		except ValueError:
+			return datetime.datetime.strptime(value,str("%"+format.lower()))
